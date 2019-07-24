eb565d86fcdf6878f7dccfe1b5492f727e0a40d9
Steve Cauffman <sjcauffm@ncsu.edu>
2019-07-23 22:59:50 -0400

    GOT THE MAP WORKING BOIIIIII

--- geocoding_raleigh.R
+++ geocoding_raleigh.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggmap)
 library(leaflet)
+library(maps)
 
 data <- read.csv("raleigh_restaurants.csv")
 
@@ -16,10 +17,21 @@ data$lat <- locations$lat
 data$long <- locations$lon
 
 ##### MAKING THE MAP
-raleigh <- get_map(location = c(lon = -78.6382, lat = 35.8), zoom = 12, maptype = "satellite",
+raleigh_basic <- get_map(location = c(lon = -78.6382, lat = 35.8), zoom = 12, maptype = "satellite",
                    source = "google")
 
 restaurant_map <- ggmap(raleigh) + 
   geom_point(aes(x = long, y = lat, colour = cusine), data = data)
 
 ##### Making interactive map
+map <- map("state", fill = TRUE, plot = FALSE)
+
+raleigh <- leaflet(data) %>%
+  addTiles() %>%
+  addMarkers(~long, ~lat, popup = paste("Name:", data$name, "<br>",
+                                        "Rating:", data$rating, "<br>",
+                                        "Reviews:", data$reviews, "<br>",
+                                        "Address:", data$address, "<br>",
+                                        "Phone Number:", data$phone_number, "<br>",
+                                        "Price:", data$price, "<br>",
+                                        "Cuisine:", data$cusine))
